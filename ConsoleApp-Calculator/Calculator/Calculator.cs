a0a2aa088c483ae5754d711a17594e3f33421445
jacek <ola88mazurek@gmail.com>
2021-08-31 20:46:50 +0200

    application - calculator

---
+++ ConsoleApp-Calculator/Calculator/Calculator.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp_Calculator
+{
+    class Calculator<T>
+    {
+        public Calculator()
+        {
+            var example = default(T);
+            var isInt = (example is int);
+            var isFloat = (example is float);
+            var isDouble = (example is double);
+
+            if (isInt)
+            {
+
+            }
+            else if (isFloat)
+            {
+
+            }
+            else if (isDouble)
+            {
+
+            }
+            else
+            {
+                Console.WriteLine("Zainicjowano kalkulator niepoprawnie!");
+            }
+        }
+
+        public T Add(T first, T second)
+        {
+            dynamic firstNumber = first;
+            dynamic secondNumber = second;
+            return firstNumber + secondNumber;
+        }
+
+        public T Substract(T first, T second)
+        {
+            dynamic firstNumber = first;
+            dynamic secondNumber = second;
+            return firstNumber - secondNumber;
+        }
+
+        public T Multyply(T first, T second)
+        {
+            dynamic firstNumber = first;
+            dynamic secondNumber = second;
+            return firstNumber * secondNumber;
+        }
+
+        public int Divide(T first, T second)
+        {
+            dynamic firstNumber = first;
+            dynamic secondNumber = second;
+            return firstNumber / secondNumber;
+        }
+    }
+}
---
+++ ConsoleApp-Calculator/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ConsoleApp_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var calculator = new Calculator<int>();
+
+            Console.WriteLine("Wpisz proszę dwie liczby oddzielone enterem: ");
+
+            var firstNumber = int.Parse(Console.ReadLine());
+            var secondNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Wpisz proszę działanie, które chcesz wykonać.");
+            Console.WriteLine("Dostępne działania to: + - * /");
+
+            var operation = Console.ReadLine();
+
+            var result = default(int);
+
+            switch (operation)
+                {
+                case "+":
+                    {
+                        result = calculator.Add(firstNumber, secondNumber);
+                    } break;
+                case "-":
+                    {
+                        result = calculator.Substract(firstNumber, secondNumber);
+                    } break;
+                case "*":
+                    {
+                        result = calculator.Multyply(firstNumber, secondNumber);
+                    } break;
+                case "/":
+                    {
+                        result = calculator.Divide(firstNumber, secondNumber);
+                    } break;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
